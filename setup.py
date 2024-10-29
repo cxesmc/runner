3858c82f15fc6d75a3218db3a79e144b849baa49
Alexander Robinson <alexander.robinson@awi.de>
2024-10-29 11:53:19 +0100

    Updated dependencies to include pandas and tabulate.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from  distutils.core import setup
+from setuptools import setup
 import versioneer
 
 setup(name='runner',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
+      author='Mahe Perrette, Alexander Robinson',
       author_email='mahe.perrette@pik-potsdam.de',
       packages = ['runner', 'runner.lib', 'runner.ext', 'runner.tools', 'runner.job'],
-      depends = ['numpy', 'scipy', 'six', 'tox','tabulate'],
+      install_requires = ['numpy', 'pandas', 'scipy', 'six', 'tox','tabulate'],
       scripts = ['scripts/job','scripts/jobrun'], 
       )
